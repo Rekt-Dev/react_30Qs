f3f048e93173ddfcc80abc50dc8227edd01ccbf4
rektDev <rektdevvv@gmail.com>
2022-09-13 05:49:49 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,13 @@ export default function App() {
   useEffect(()=> {
     fetch("https://jsonplaceholdere.typicode.com/users")
     .then ((response)=> response.json())
-  })
+    .then((json)=> console.log(json))
+  },[])
   
-  return <div>
-    
+  return <div className="App">
+    <h1>
+      Users
+    </h1>
     
   </div>;
 }
