55f3cec18f66b3dec81040a2f5e9583ab79e445f
rektDev <rektdevvv@gmail.com>
2022-09-13 05:45:29 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import "./styles.css";
 import {useEffect} from "react"
 
 export default function App() {
+  useEffect(()=> {
+    fetch("https://jsonplaceholdere.typicode.com/users")
+    .then ((response)=> response.json())
+  })
+  
   return <div>
     
     
