4e537c999182aae863f574e9d8635f06402a7a62
rektDev <rektdevvv@gmail.com>
2022-09-13 06:06:22 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import "./styles.css";
-import {useEffect} from "react"
+import {useEffect, useState} from "react"
 
 export default function App() {
+  const[users, sertUsers] = useState([])
   useEffect(()=> {
     fetch("https://jsonplaceholdere.typicode.com/users")
     .then ((response)=> response.json())
@@ -13,10 +14,15 @@ export default function App() {
       Users
     </h1>
     <div className="card">
+      {users.map((user)=>(
+       <div>
+         
+       </div> 
+      ))}
       <div className="card-inner">
         <p>Ofear</p>
         <p>Ofear888</p>
-  </div>;
+  </div>
   </div>
   </div>
 }
